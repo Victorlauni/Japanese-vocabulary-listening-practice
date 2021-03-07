decb951678684056e26e10fb13847a9853dade0a
Victor Lau <vtleluc@gmail.com>
2021-03-07 21:32:18 +0800

    Initial Commit

---
+++ Japanese-listening-pratice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Japanese-listening-pratice
+//
+//  Created by Victor Lau on 7/3/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Japanese-listening-pratice/Japanese_listening_praticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Japanese_listening_praticeApp.swift
+//  Japanese-listening-pratice
+//
+//  Created by Victor Lau on 7/3/2021.
+//
+
+import SwiftUI
+
+@main
+struct Japanese_listening_praticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
