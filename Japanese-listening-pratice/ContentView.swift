3d3026d36f42d038df8b21f308dfab973b9a5681
Victor Lau <vtleluc@gmail.com>
2021-03-13 07:01:50 +0800

    Completed basic function - import .txt file - parse and shuffle the imported file - read the text in the wordlist - check input answer with the word

--- Japanese-listening-pratice/ContentView.swift
+++ Japanese-listening-pratice/ContentView.swift
@@ -6,16 +6,128 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+func openFile(wordList: wordlist) -> Void {
+    wordList.list.removeAll()
+    let dialog = NSOpenPanel()
+    dialog.allowedFileTypes = ["txt"]
+    dialog.allowsMultipleSelection = false
+    dialog.canChooseDirectories = false
+    if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+        let url = dialog.url
+        // print(url ?? "")
+        if let unwrappedURL = url {
+            let FS = FileManager()
+            guard let data = FS.contents(atPath: unwrappedURL.path) else {return}
+            guard let dataString = String(data: data, encoding: .utf8) else {
+                return
+            }
+            let stringList = dataString.split(separator: "\n")
+            for str in stringList {
+                wordList.list.append(String(str))
+            }
+            wordList.list.shuffle()
+        }
+    }
+}
 
 struct ContentView: View {
+    @EnvironmentObject var wordList: wordlist
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if (wordList.list.isEmpty) {
+            OpenFileView()
+        }
+        else {
+            MainView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct OpenFileView: View {
+    @EnvironmentObject var wordList: wordlist
+    var body: some View {
+        Button("Open File") {
+            openFile(wordList: wordList)
+        }
     }
 }
+
+struct MainView: View {
+    @EnvironmentObject var wordList: wordlist
+    @State var question: Int = 0
+    @State private var answerVis = false
+    @State private var answer: String = ""
+    @State private var wrongAns = false
+    func playSound() {
+        guard question < wordList.list.count else {return}
+        let utt = AVSpeechUtterance(string: wordList.list[question])
+        utt.voice = AVSpeechSynthesisVoice(language: "ja")
+        utt.rate = 0.1
+        let syn = AVSpeechSynthesizer()
+        syn.speak(utt)
+    }
+    
+    func checkAns() {
+        if (answer != wordList.list[question]) {
+            wrongAns = true
+            answer = ""
+        }
+        else {
+            answerVis = false
+            question += 1
+            answer = ""
+        }
+    }
+    
+    
+    var body: some View {
+        VStack {
+            //Text(wordList.list[question])
+            Text("Test").opacity(answerVis ? 1 : 0)
+            HStack {
+                Button("Listen", action: playSound)
+                Button("Show Answer") {
+                    answerVis = true
+                }
+            }
+            Divider()
+            HStack {
+                TextField("Answer", text: $answer,
+                          onCommit: {
+                            checkAns()
+                          }
+                    )
+                    .border(Color.red, width: wrongAns ? 2 : 0)
+                Button("Submit") {
+                    checkAns()
+                }
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct Menubar : Commands {
+    var wordList: wordlist
+
+    
+    var body: some Commands {
+        CommandGroup(replacing: CommandGroupPlacement.newItem) {
+            Button("Open File") {
+                openFile(wordList: wordList)
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews:
+    PreviewProvider {
+    static var Preview_wordList = wordlist()
+    static var previews: some View {
+        MainView().frame(width: 300.0, height: 300.0).environmentObject(Preview_wordList)
+    }
+}
+
--- Japanese-listening-pratice/Japanese_listening_praticeApp.swift
+++ Japanese-listening-pratice/Japanese_listening_praticeApp.swift
@@ -7,11 +7,20 @@
 
 import SwiftUI
 
+class wordlist: ObservableObject {
+    @Published var list: [String] = []
+}
+
 @main
 struct Japanese_listening_praticeApp: App {
+    let wordList = wordlist()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(wordList)
+                .frame(minWidth: 100, idealWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxWidth: 300, minHeight: 100, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 300, alignment: .center)
+        }.commands() {
+            Menubar(wordList: wordList)
         }
     }
 }
